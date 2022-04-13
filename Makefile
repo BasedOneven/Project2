030f73cbeb391e16a2d7ae2a6e7426975cbea3a1
KT <ktyalplummer@gmail.com>
2022-04-13 19:31:48 -0400

    tests 1 and 2 pass

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ CC=gcc
 CFLAGS=-I.
 
 wzip: wzip.o
-	$(CC) -o wzip wzip.o
+	$(CC) -o wzip wzip.o -Wall -Werror
--- wzip.c
+++ wzip.c
@@ -1,7 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
 int main(int argc, char *argv[])
 {
-    printf("Hello, World!\n");
+    // no files error 
+    
+    // combine all input files into a single file
+    // create new file for combination of inputs 
+    FILE *combinedFile = fopen("combined.txt", "w");
+
+    // grab all files from the command line and put them in the file
+    for(int i = 1; i < argc; i++) {
+        char currentChar;
+        FILE *fp = fopen(argv[i], "r");
+        if(fp == NULL) {
+            exit(1);
+        }
+        
+        // store characters from the current file into the combined file
+        while((currentChar = fgetc(fp)) != EOF)
+        {
+            fputc(currentChar, combinedFile);
+        }
+        fclose(fp);
+    }
+    fclose(combinedFile);
+    
+    // convert characters from the combined file to binary
+    char currentChar;
+    int cur = 1;
+    FILE *fp = fopen("combined.txt", "r");
+    if(fp == NULL) {
+        exit(1);
+    }
+        
+    char tempChar = fgetc(fp);
+    while(tempChar != EOF)
+    {
+        currentChar = fgetc(fp);
+        if(tempChar == currentChar)
+        {
+            cur++;
+        }
+        else 
+        {
+            fwrite(&cur, 4, 1, stdout);
+            fwrite(&tempChar, 1, 1, stdout);
+            tempChar = currentChar;
+            cur = 1;
+        }
+    }
+    fclose(fp);
 }
